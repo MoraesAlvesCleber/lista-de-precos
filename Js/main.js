5d1c92aa64a1e62a57c2d1d8fbb8cdb92c74c61f
Cleber Moraes <alvesmoraesb@gmail.com>
2023-01-07 09:10:20 -0300

    Validando imput
    Inserindo condições para preenchimento do imput

--- Js/main.js
+++ Js/main.js
@@ -8,6 +8,11 @@ class Produto {
 
     salvar() {
         let produto = this.lerDados();
+
+        if(this.validaCampos(produto)) {
+            alert("Salvar");
+        }
+        
         console.log(produto);
     }
 
@@ -20,7 +25,24 @@ class Produto {
         produto.preco = document.getElementById('preco').value;
 
         return produto;
+    }
+    validaCampos(produto){
+        let msg = "";
 
+        if (produto.nomeDoProduto == "") {
+            msg += " - Informe o nome do produto \n";
+        }
+        if (produto.nomeDaLoja == "") {
+            msg += " - Informe o nome da loja \n";
+        }
+        if (produto.preco == "") {
+            msg += " - Informe o preço do produto \n";
+        }
+        if (msg != ""){
+            alert(msg);
+            return false;
+        }
+        return true;
     }
     Excluir() {
         alert('Excluir produto')
