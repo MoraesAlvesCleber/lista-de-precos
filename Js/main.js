33f2d3915832b4b2b96ee2e9bd83a7c01b4b2f2a
Cleber Moraes <alvesmoraesb@gmail.com>
2023-01-08 21:26:16 -0300

    Inserindo imagens e funções
    Inserindo imagem deletar, adicionando função deletar para remover itens do array. Listando array na tabela.

--- Js/main.js
+++ Js/main.js
@@ -3,44 +3,74 @@ class Produto {
     constructor() {
         this.id = 1;
         this.arrayProdutos = [];
+       
 
     }
+    
 
     salvar() {
         let produto = this.lerDados();
 
-        if(this.validaCampos(produto)) {
+        if (this.validaCampos(produto)) {
             this.adicionar(produto);
-             
-        }
-        
-         this.listaTabela();
-    }
 
-    listaTabela(){
+        }
+
+        this.listaTabela();
+        this.apagar();
+        
+    }
+    
+
+    listaTabela() {
         let tbody = document.getElementById("tbody");
         tbody.innerText = "";
 
-        for(let i= 0 ; i < this.arrayProdutos.length; i++) {    
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
             let tr = tbody.insertRow();
 
             let td_id = tr.insertCell();
             let td_loja = tr.insertCell();
             let td_produto = tr.insertCell();
             let td_preco = tr.insertCell();
+            let td_apagar = tr.insertCell();
+
 
             td_id.innerText = this.arrayProdutos[i].id;
             td_loja.innerText = this.arrayProdutos[i].nomeDaLoja;
             td_produto.innerText = this.arrayProdutos[i].nomeDoProduto;
             td_preco.innerText = this.arrayProdutos[i].preco;
 
+            td_id.classList.add("center");
+
+            let imgEdit = document.createElement('img');
+            imgEdit.src = 'img/delete.svg';
+            imgEdit.setAttribute('onclick', 'produto.deletar(' + this.arrayProdutos[i].id + ')');
+
+            td_apagar.appendChild(imgEdit);
+            
+
             
         }
+        
     }
+   
 
     adicionar(produto) {
         this.arrayProdutos.push(produto);
         this.id++;
+        const itens = []
+
+        const itemAtual = {
+            'nomeDaLoja' : produto.nomeDaLoja,
+            'nomeDoProduto' : produto.nomeDoProduto,
+            'preco' : produto.preco
+        }
+        itens.push(itemAtual);
+
+        localStorage.setItem('item',JSON.stringify(itens))
+         
+      
     }
 
     lerDados() {
@@ -53,7 +83,7 @@ class Produto {
 
         return produto;
     }
-    validaCampos(produto){
+    validaCampos(produto) {
         let msg = "";
 
         if (produto.nomeDoProduto == "") {
@@ -65,13 +95,27 @@ class Produto {
         if (produto.preco == "") {
             msg += " - Informe o preço do produto \n";
         }
-        if (msg != ""){
+        if (msg != "") {
             alert(msg);
             return false;
         }
         return true;
     }
+    apagar() {
+        document.getElementById('loja').value = '';
+        document.getElementById('produto').value = '';
+        document.getElementById('preco').value = '';
 
-   
+    }
+    deletar(id) {
+        let tbody = document.getElementById("tbody");
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            if (this.arrayProdutos[i].id == id) {
+                this.arrayProdutos.splice(i, 1);
+                tbody.deleteRow(i);
+            }
+        }
+
+    }
 }
 var produto = new Produto();
